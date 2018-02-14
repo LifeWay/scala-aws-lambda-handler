a73658419b293da9fd5746811027180bf51f1d77
Ryan Means <ryan.means@lifeway.com>
2018-02-14 14:34:40 -0600

    WIP - updates.

--- src/main/scala/com/lifeway/aws/lambda/LambdaProxy.scala
+++ src/main/scala/com/lifeway/aws/lambda/LambdaProxy.scala
@@ -10,6 +10,14 @@ import org.slf4j.LoggerFactory
 
 import scala.io.Source
 
+trait LambdaProxyBase {
+  def encode[T](obj: APIGatewayProxyResponse[T])(implicit encoder: Encoder[T],
+                                                 stringEncoder: Encoder[String]): String = {
+    val bodyAsJsonString = obj.body.map(_.asJson.noSpaces)
+    obj.copy[String](body = bodyAsJsonString).asJson(APIGatewayProxyResponse.encode[String]).noSpaces
+  }
+}
+
 /**
   * Abstract Lambda Handler for Scala. You must extend this class and implement the handler and invalidInput methods.
   * Additionally, you must provide a Circe decoder for your Input type and Circe encoders for your Failure type and
@@ -24,16 +32,14 @@ import scala.io.Source
   */
 abstract class LambdaProxy[I, F, S](implicit inputDecoder: Decoder[I],
                                     failureEncoder: Encoder[F],
-                                    successEncoder: Encoder[S]) {
+                                    successEncoder: Encoder[S])
+    extends LambdaProxyBase {
   implicit val reqDecoder: Decoder[APIGatewayProxyRequest[I]] = APIGatewayProxyRequest.decode[I](inputDecoder)
   private val logger                                          = LoggerFactory.getLogger("BASE_HANDLER")
 
-  def handler(request: APIGatewayProxyRequest[I],
-              c: Context): Either[APIGatewayProxyResponse[F], APIGatewayProxyResponse[S]]
+  def handler(request: APIGatewayProxyRequest[I], c: Context): LambdaProxy.Response[F, S]
 
-  def invalidInput(circeError: Error): APIGatewayProxyResponse[F]
-
-  final def handler(is: InputStream, os: OutputStream, context: Context): Unit = {
+  final def handler[T](is: InputStream, os: OutputStream, context: Context): Unit = {
     val inputString = Source.fromInputStream(is).mkString
     logger.debug(s"Lambda Proxy Input: $inputString")
     val input = decode[APIGatewayProxyRequest[I]](inputString)
@@ -50,9 +56,16 @@ abstract class LambdaProxy[I, F, S](implicit inputDecoder: Decoder[I],
     os.close()
   }
 
-  private def encode[T](obj: APIGatewayProxyResponse[T])(implicit encoder: Encoder[T],
-                                                         stringEncoder: Encoder[String]): String = {
-    val bodyAsJsonString = obj.body.map(_.asJson.noSpaces)
-    obj.copy[String](body = bodyAsJsonString).asJson(APIGatewayProxyResponse.encode[String]).noSpaces
-  }
+  def invalidInput(circeError: Error): APIGatewayProxyResponse[F]
+}
+
+abstract class LambdaProxyNoReqBody[F, S](implicit
+                                          failureEncoder: Encoder[F],
+                                          successEncoder: Encoder[S])
+    extends LambdaProxy[None.type, F, S]
+
+object LambdaProxy {
+
+  type Response[F, S] = Either[APIGatewayProxyResponse[F], APIGatewayProxyResponse[S]]
+
 }
